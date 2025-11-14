0638b0c80f7e581bb53107cd560aaec27bbbee1b
KwonYongBin <dydqls6789@naver.com>
2025-11-14 10:30:27 +0900

    test

---
+++ map-test/src/App.js
@@ -0,0 +1,10 @@
+import Map from './pages/Map'
+function App() {
+  return (
+    <>
+      <Map />
+    </>
+  );
+}
+
+export default App;
---
+++ map-test/src/pages/Map.jsx
@@ -0,0 +1,24 @@
+import { Map } from "react-kakao-maps-sdk"
+import useKakaoLoader from "../useKakaoLoader"
+
+export default function BasicMap() {
+  useKakaoLoader()
+
+  return (
+    <Map // 지도를 표시할 Container
+      id="map"
+      center={{
+        // 지도의 중심좌표
+        lat: 33.450701,
+        lng: 126.570667,
+      }}
+      style={{
+        // 지도의 크기
+        width: "50%",
+        height: "500px",
+        margin: "200px auto"
+      }}
+      level={3} // 지도의 확대 레벨
+    />
+  )
+}
